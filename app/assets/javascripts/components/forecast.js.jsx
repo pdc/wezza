bcc029640c963e957ad3f08efdd2a9fa75a0fb38
Damian Cugley <pdc@alleged.org.uk>
2015-08-17 23:57:20 +0100

    Add collabsilble details to forecast parts

--- app/assets/javascripts/components/forecast.js.jsx
+++ app/assets/javascripts/components/forecast.js.jsx
@@ -45,19 +45,80 @@ class ForecastSource {
 var forecastSource = new ForecastSource();
 
 
+const daysOfWeek = ['Su', 'M', 'T', 'W', 'Th', 'F', 'Sa'];
+
+
+var ForecastDetail = React.createClass({
+    getInitialState: function () {
+        return {
+            isExpanded: false,
+        };
+    },
+
+    handleClick: function (ev) {
+        ev.preventDefault();
+
+        this.setState({isExpanded: !this.state.isExpanded});
+    },
+
+    render: function () {
+        if (!this.state.isExpanded) {
+            return (
+                <div className="forecast-detail-collapsed">
+                    <h3 onClick={this.handleClick}>Show more</h3>
+                </div>);
+        }
+
+        const tableRows = this.props.data.map((r, i) => {
+            const timeDate = new Date(1000 * r.time);
+            const timeStr = (this.props.time === 'd'
+                ? daysOfWeek[timeDate.getDay()]
+                : (timeDate.getHours() < 10 ? '0' : '') + timeDate.getHours() + ':00');
+            let row = [
+                <td>{timeStr}</td>,
+                <td>{r.summary}</td>
+            ];
+            if ('temperature' in r) {
+                row.push(<td>{Math.round(r.temperature)}°</td>);
+            } else {
+                row.push(<td>{Math.round(r.temperatureMin)}°</td>);
+                row.push(<td>{Math.round(r.temperatureMax)}°</td>);
+            }
+            return (<tr key={i} className={r.icon}>
+                {row}
+            </tr>);
+        });
+        return (
+            <div className="forecast-detail-expanded">
+                <h3 onClick={this.handleClick}>Show less</h3>
+                <table>
+                    <tbody>
+                        {tableRows}
+                    </tbody>
+                </table>
+            </div>);
+    },
+});
+
+
 var ForecastPart = React.createClass({
     render: function () {
         const data = this.props.data;
-        const temperaturePart = (data.temperature &&
+        const temperatureData = (data.temperature
+            ? data : data.data[0]);
+        const temperaturePart = (temperatureData.temperature &&
             <div className="forecast-part-temperature">
-                <b>{data.temperature}°</b>
-                <small>Feels like {data.apparentTemperature}°</small>
+                <b>{Math.round(temperatureData.temperature)}°</b>
+                <small>Feels like {Math.round(temperatureData.apparentTemperature)}°</small>
             </div>);
+        const isDetail = (data.data
+            && ('temperature' in data.data[0] || 'temperatureMin' in data.data[0]));
         return (
-            <div className={'forecast-part forecast-part-' + data.icon}>
+            <div className={'forecast-part ' + data.icon}>
                 <h2>{this.props.title}</h2>
                 <div className="forecast-part-summary">{data.summary}</div>
                 {temperaturePart}
+                {isDetail && <ForecastDetail data={data.data} time={this.props.time}/>}
             </div>);
     },
 })
@@ -87,7 +148,7 @@ var Forecast = React.createClass({
 
     render: function () {
         if (this.state.isLoading) {
-            return <div className="forecast forecast-loading">LOADING…</div>;
+            return <div className="forecast forecast-loading"></div>;
         }
 
         const forecast = this.state.forecast;
@@ -106,8 +167,8 @@ var Forecast = React.createClass({
             <div className='forecast'>
                 <ForecastPart key="currently" title="Currently" data={forecast.currently} />
                 {forecast.minutely && <ForecastPart key="hour" title="Next hour" data={forecast.minutely} />}
-                <ForecastPart key="hourly" title="Next day" data={forecast.hourly} />
-                <ForecastPart key="daily" title="Next week" data={forecast.daily} />
+                <ForecastPart key="hourly" title="Next day" data={forecast.hourly} time="hm" />
+                <ForecastPart key="daily" title="Next week" data={forecast.daily} time="d" />
             </div>);
     },
 });
